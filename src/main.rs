0fd069f46bc658e28d9f0724e85eb21808c5596e
Lucas Tanne <lucas.tanne29@gmail.com>
2026-02-01 12:04:16 +0100

    feat: Can verify the PNG signature

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
 use std::{fs::File, io::{BufRead, BufReader}};
 
+// See https://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
+const PNG_SIGNATURE: [u8; 8] = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+
 fn main() {
     println!("Starting image reader...");
 
-    let file_path = "./cat.jpg";
+    let file_path = "./cat.png";
 
     let file = match File::open(file_path) {
         Ok(file) => file,
         Err(err) => panic!("Could not open the file: {}, err: {}", file_path, err)
     };
-
-    // READ as string
-
-    // let mut content = String::new();
-    // let content_size = match file.read_to_string(&mut content) {
-    //     Ok(size) => size,
-    //     Err(err) => panic!("Could not read file content, got error: {}", err)
-    // };
-
-    // println!("File content size: {}", content_size);
-
-    // println!("File content: {}", content);
-
-    // READ as byte buf
 
     let mut reader = BufReader::new(file);
     let content = match reader.fill_buf() {
@@ -32,8 +21,15 @@ fn main() {
 
     println!("File content size: {}", content.len());
 
-    for b in content {
-        print!("{} ", b)
+    // // Print the byte stream
+    // for b in content {
+    //     print!("{} ", b)
+    // }
+
+    // Compare PNG signature
+    let file_signature = &content[..8];
+    if !file_signature.cmp(&PNG_SIGNATURE).is_eq() {
+        panic!("This is not a PNG file, signature doesn't match");
     }
 
 }
